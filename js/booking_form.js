5db31e3278db79edff9e5d4183fc25fc0f828737
Bugkahuna <preps@rambler.ru>
2021-03-14 14:06:41 +0300

    Исправления 2 итерация

--- js/booking_form.js
+++ js/booking_form.js
@@ -1,12 +1,9 @@
 const popup= document.querySelector('.search-button');
 const modal = document.querySelector('.modal-window');
-popup.addEventListener("click", function (evt) {
+popup.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modal.classList.toggle("modal-show");
+  modal.classList.toggle('modal-show');
 });
 
-const close_popup = document.querySelector(".find-button");
-close_popup.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modal.classList.remove('modal-show');
-});
+modal.classList.remove('modal-show');
+
